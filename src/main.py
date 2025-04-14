da1b6f286c2250a86a95899e4f85481d09adf02e
johnsydev <johnsylopez02@gmail.com>
2025-04-14 11:03:56 -0600

    Créditos

--- src/main.py
+++ src/main.py
@@ -70,6 +70,15 @@ def main(page: ft.Page):
                             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                             spacing=150,
                         )
+                    ),
+                    ft.Container(
+                        alignment=ft.alignment.bottom_right,
+                        content=ft.FloatingActionButton(
+                            icon=ft.Icons.INFO,
+                            bgcolor=ft.Colors.with_opacity(0.5, '#182C61'), 
+                            on_click=lambda e: page.go('/creditos')
+                        ),
+                        padding=20
                     )
                 ]
             )
@@ -187,6 +196,61 @@ def main(page: ft.Page):
         
         elif page.route == "/laberinto":
             page.views.append(pantalla_laberinto(page))
+
+        elif page.route == "/creditos":
+            page.views.append(
+                ft.View(
+                    '/creditos',
+                    [
+                        ft.Stack(
+                            controls=[
+                                ft.Container(
+                                    expand=True,
+                                    alignment=ft.alignment.center,
+                                    content=ft.Column(
+                                        controls=[
+                                            ft.Row(
+                                                controls=[
+                                                    ft.Icon(name=ft.Icons.CODE, size=96, color=ft.Colors.with_opacity(0.75, '#182C61')),
+                                                    ft.Text('Creditos', size=96, weight=ft.FontWeight.BOLD, font_family='Jersey 25'),
+                                                    ft.Icon(name=ft.Icons.CODE_SHARP, size=96, color=ft.Colors.with_opacity(0.75, '#182C61'))
+                                                ],
+                                                alignment=ft.MainAxisAlignment.CENTER
+                                            ),
+                                            ft.Divider(),
+                                            ft.Text('Desarrolladores', size=48, weight=ft.FontWeight.BOLD, font_family='Jersey 25'),
+                                            ft.Text('Sergio Montoya Badilla', size=24, weight=ft.FontWeight.BOLD, font_family='Jersey 25'),
+                                            ft.Text('Johnsy Lopez Aguilar', size=24, weight=ft.FontWeight.BOLD, font_family='Jersey 25'),
+                                            ft.Text(' ', size=24, weight=ft.FontWeight.BOLD, font_family='Jersey 25'),
+                                            ft.Text('Profesor', size=48, weight=ft.FontWeight.BOLD, font_family='Jersey 25'),
+                                            ft.Text('Jose Angel Campos Aguilar', size=24, weight=ft.FontWeight.BOLD, font_family='Jersey 25'),
+                                            ft.Text(' ', size=24, weight=ft.FontWeight.BOLD, font_family='Jersey 25'),
+                                            ft.Text('Creado por estudiantes del TEC con mucho sueño en Semana Santa', size=24, weight=ft.FontWeight.BOLD, font_family='Jersey 25'),
+                                            ft.Text(' ', size=24, weight=ft.FontWeight.BOLD, font_family='Jersey 25'),
+                                            ft.Text('Laberinto limonense. Todos los derechos reservados 2025.', size=14, weight=ft.FontWeight.BOLD, font_family='Jersey 25'),
+                                        ],
+                                        alignment=ft.MainAxisAlignment.CENTER,
+                                        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                                        spacing=35,
+                                    )
+                                ),
+                                ft.Container(
+                                    content=ft.FloatingActionButton(
+                                        icon=ft.icons.ARROW_BACK,
+                                        on_click=lambda _: page.go('/'),
+                                        bgcolor=ft.Colors.with_opacity(0.5, '#182C61'),
+                                        shape=ft.CircleBorder(),
+                                        scale=1.5
+                                    ),
+                                    alignment=ft.alignment.top_left,
+                                    margin=30
+                                )
+                            ]
+                        )
+                    ]
+                )
+            )
+
         
         page.update()
 
