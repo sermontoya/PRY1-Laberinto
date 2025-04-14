d4096971da84a206c34f23c58877120af76e425c
sermontoya <sermontoya@estudiantec.cr>
2025-04-13 22:31:34 -0600

    Solución de laberintos

--- Laberintos.py
+++ Laberintos.py
@@ -142,3 +142,63 @@ def cantUnos(matriz):
             if j==1:
                 resultado+=1
     return resultado
+
+def solucionarLaberinto(matriz, inicioX,inicioY, finX, finY):
+    mejorSolucion=[]
+    cantPasosMejorMatriz=100
+    tamano=len(matriz)
+    for i in range(2): 
+        matrizTemporal=matriz 
+        if solucionarLaberinto_aux(tamano, matrizTemporal, inicioX, inicioY, finX, finY, inicioX, inicioY):
+            cantidadPasos = cantCuatros(matrizTemporal)
+            if cantidadPasos < cantPasosMejorMatriz:
+                cantPasosMejorMatriz = cantidadPasos
+                mejorSolucion = matrizTemporal
+    if mejorSolucion==[]:
+        return -1
+    return mejorSolucion
+
+
+def solucionarLaberinto_aux(tamano, matriz, inicioX, inicioY, finX, finY, posX, posY):
+    if posX==finX and posY==finY:
+            return True 
+    if matriz[posX][posY]!=1:
+            return False
+    matriz[posX][posY]=4
+    
+    #0 que viene de izquierda, 1 que viene de derecha, 2 que viene de arriba, 3 que viene de abajo
+    listaAux=[0, 1, 2, 3]
+    random.shuffle(listaAux)
+    for aux in listaAux:
+        nuevaX = posX
+        nuevaY= posY
+        if aux == 0:
+            nuevaX -= 1
+        elif aux == 1:
+            nuevaX += 1
+        elif aux == 2:
+            nuevaY -= 1
+        elif aux == 3:
+            nuevaY += 1
+        if comprobarPosicionValida(tamano, nuevaX, nuevaY):
+            if solucionarLaberinto_aux(tamano, matriz, inicioX, inicioY, finX, finY, nuevaX, nuevaY):
+                return True
+    matriz[posX][posY]=1
+    return False
+
+
+def cantCuatros(matriz):
+    resultado=0
+    for i in matriz:
+        for j in i:
+            if j==4:
+                resultado+=1
+    return resultado
+
+matriz=[[1,1,1,1,1],
+        [0,1,0,0,1],
+        [0,1,0,0,1],
+        [1,1,1,0,1],
+        [1,0,1,1,1]]
+print(solucionarLaberinto(matriz, 1, 0, 4, 4))
+
